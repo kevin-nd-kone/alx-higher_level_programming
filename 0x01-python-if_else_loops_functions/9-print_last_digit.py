4fef92e92775d8f440d66b7b8ba5ebc320358610
kevinDlan <kevinkone19@gmail.com>
2022-08-23 22:47:16 +0000

    task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    num = 0
+    if(number < 0):
+        num = number * (-1)
+        last = (num % 10)*(-1)
+    else:
+        last = number % 10
+
+    print("{}".format(last), end="")
+    return last
