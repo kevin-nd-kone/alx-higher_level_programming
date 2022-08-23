e6145cdc5e672446fe7d4032bc5d4c78298f965c
kevinDlan <kevinkone19@gmail.com>
2022-08-23 23:27:23 +0000

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 != 0:
@@ -9,5 +10,3 @@ def fizzbuzz():
             print("FizzBuzz", end=" ")
         else:
             print("{:d}".format(i), end=" ")
-
-fizzbuzz()
