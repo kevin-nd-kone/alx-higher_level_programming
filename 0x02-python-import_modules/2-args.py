a9ae97dc83594e70c09e5a795cc9f05ac02d5ede
kevinDlan <kevinkone19@gmail.com>
2022-08-25 23:24:18 +0000

    task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arg = len(sys.argv)
+    if arg == 1:
+        print("{:d} {}".format(0, "argments"))
+    elif arg == 2:
+        print("{:d} {}".format(1, "argument"))
+        for index in range(1, arg):
+            print("{:d}: {}".format(arg - 1, sys.argv[index]))
+    else:
+        print("{:d} {}".format(arg - 1, "arguments"))
+        for index in range(1, arg):
+            print("{:d}: {}".format(arg, sys.argv[index]))
