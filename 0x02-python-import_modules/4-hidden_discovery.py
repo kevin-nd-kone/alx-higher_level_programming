a089434fcf378e5ea56402cd94edb22f36a472e6
kevinDlan <kevinkone19@gmail.com>
2022-08-26 00:25:32 +0000

    task 6

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ from unicodedata import name
 import hidden_4
 if __name__ == "__main__":
     for val in dir(hidden_4):
-        if "__" not in name:
-            print(name)
+        if "__" not in val:
+            print(val)
