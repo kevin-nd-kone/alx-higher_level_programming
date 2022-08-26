9bfe6e4627287c17d8d45e981d47f9b86d092916
kevinDlan <kevinkone19@gmail.com>
2022-08-26 00:05:53 +0000

    handle some bug

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "main":
+if __name__ == "__main__":
     arg = len(sys.argv)
     s = 0
     for num in range(1, arg):
-        code = code(sys.argv[num])
-        if code.isdigit():
+        argument = sys.argv[num]
+        if argument.isdigit():
             s += int(sys.argv[num])
     print(s)
