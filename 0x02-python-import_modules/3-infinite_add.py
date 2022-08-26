97d890ee27d7f407e9bb8ea2524deaa727fed567
kevinDlan <kevinkone19@gmail.com>
2022-08-26 00:15:01 +0000

    task 5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,5 @@ if __name__ == "__main__":
     arg = len(sys.argv)
     s = 0
     for num in range(1, arg):
-        argument = sys.argv[num]
-        if argument.isdigit():
-            s += int(sys.argv[num])
+        s += int(sys.argv[num])
     print(s)
