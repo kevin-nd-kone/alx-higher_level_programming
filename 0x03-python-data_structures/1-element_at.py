587f7ca746a49754ae14b432668c560e9894f40c
kevinDlan <kevinkone19@gmail.com>
2022-08-28 22:19:46 +0000

    update some error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,3 +3,8 @@ def element_at(my_list, idx):
     if len(my_list) < idx or idx < 0:
         return None
     return my_list[idx]
+
+
+my_list = [1, 2, 3, 4, 5]
+idx = 10
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
