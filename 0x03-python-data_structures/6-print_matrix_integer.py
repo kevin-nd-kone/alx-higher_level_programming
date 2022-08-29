da57f6af227c953054ca13ac8088e41351c3095f
kevinDlan <kevinkone19@gmail.com>
2022-08-29 00:20:17 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        print("{}".format("$"))
+        print("{}".format(""))
     else:
         for line in matrix:
             row = ""
             for num in line:
                 row += ' {}'.format(num)
-            print("{}{}".format(row, "$"))
+            print("{}".format(row))
