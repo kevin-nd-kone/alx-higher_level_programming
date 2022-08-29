e39ff1c35124b6bbc309f9aca80d5af0dafcee3b
kevinDlan <kevinkone19@gmail.com>
2022-08-29 01:13:24 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,12 +3,7 @@ def multiple_returns(sentence):
     length = 0
     first_char = None
     if sentence == "":
-        return length,first_char
+        return length, first_char
     length = len(sentence)
     first_char = sentence[0]
-    return length,first_char
-
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+    return length, first_char
