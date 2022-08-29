3cae92ced6e49f20f095c63845f2dba02bd7d981
kevinDlan <kevinkone19@gmail.com>
2022-08-29 01:42:09 +0000

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    bool_list = []
+    for elt in my_list:
+        if elt % 2 == 0:
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    return bool_list
