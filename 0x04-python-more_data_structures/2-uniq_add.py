204877a19f714a802fc97f02ee26a1b7f17cfaf2
kevinDlan <kevinkone19@gmail.com>
2022-08-31 23:09:37 +0000

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,13 +3,8 @@ def uniq_add(my_list=[]):
     sum = 0
     tab = []
     for num in my_list:
-        if num not in tab:
+        if num in tab:
             tab.append(num)
     for elt in tab:
         sum += elt
     return sum
-
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
