91624794c44e3b92ec73f884e8d887c8160fe653
kevinDlan <kevinkone19@gmail.com>
2022-08-31 23:47:58 +0000

    task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     keys_tab = []
-    for k,v in a_dictionary.items():
+    for k, v in a_dictionary.items():
         keys_tab.append(k)
     return len(keys_tab)
