2d3389378d003c1a886243f656c321b8fc9c3530
kevinDlan <kevinkone19@gmail.com>
2022-09-17 22:29:38 +0000

    first task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,20 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         newList = my_list[:x]
-        v = 0
-        for i in newList: v +=1
+        l = 0
+        for i in newList:
+            l += 1
         print(''.join(str(i) for i in newList))
-        return v
+        return l
     except:
         print()
-
-
-my_list = [1,2,3,4,5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
 
