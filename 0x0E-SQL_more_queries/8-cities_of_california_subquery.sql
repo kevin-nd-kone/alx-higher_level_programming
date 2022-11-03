dca773f9652a7f5c25d8810df4fea42177b39653
kevinDlan <kevinkone19@gmail.com>
2022-11-03 23:40:21 +0000

    1

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- SELECT all cities of californie
 SELECT id, state_id, name 
          FROM cities
-         WHERE cities.state_id = ( SELECT id FROM states WHERE name = 'California')
+         WHERE cities.state_id  IN ( SELECT id FROM states WHERE name = 'California')
          ORDER BY cities.id ASC;
