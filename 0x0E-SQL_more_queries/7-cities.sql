7ff275708fe1318c620c5c4fb64c97f1617a6919
kevinDlan <kevinkone19@gmail.com>
2022-11-03 23:10:53 +0000

    1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,5 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE Table IF NOT EXISTS cites(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
-                                state_id int NOT NULL FOREIGN KEY REFERENCES states ON id,
+                                state_id int NOT NULL,
+                                FOREIGN KEY(state_id) REFERENCES states(id),
                                 name VARCHAR(256) NOT NULL);
