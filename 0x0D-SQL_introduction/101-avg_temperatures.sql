0a25b44d833faaf46118de87d349d73a316bbde3
kevinDlan <kevinkone19@gmail.com>
2022-11-03 21:28:50 +0000

    1

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Import data from sql file
+--  < https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
+SET autocommit=0 ; source https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql ; COMMIT ;
