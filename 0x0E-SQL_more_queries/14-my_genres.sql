800e06dc6a4f78c512ca1a64be4f054b9bb0d12e
kevinDlan <kevinkone19@gmail.com>
2022-11-04 01:01:58 +0000

    1

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,6 +4,6 @@ SELECT tv_genres.name FROM tv_genres
        INNER JOIN tv_show_genres 
        ON tv_genres.id = tv_show_genres.genre_id
        INNER JOIN tv_show
-       ON tv_show.id = tv_show_genres.tv_show_id
+       ON tv_show_genres.tv_show_id = tv_show.id  
        WHERE tv_show.title = 'Dexter'
        ORDER BY tv_genres.name ASC;
