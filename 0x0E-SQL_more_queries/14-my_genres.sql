c6c108e8d10822af0258d46b9d1bf79ffa6763a5
kevinDlan <kevinkone19@gmail.com>
2022-11-04 00:58:43 +0000

    1

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- 
+-- 
+SELECT tv_genres.name FROM tv_genres 
+       INNER JOIN tv_show_genres 
+       ON tv_genres.id = tv_show_genres.genre_id
+       INNER JOIN tv_show
+       ON tv_show.id = tv_show_genres.tv_show_id
+       WHERE tv_show.title = 'Dexter'
+       ORDER BY tv_genres.name ASC;
