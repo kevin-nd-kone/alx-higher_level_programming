86b25d296a35c56f20b68b7f1c4b81c4f6ab9dff
kevinDlan <kevinkone19@gmail.com>
2022-11-05 11:28:22 +0000

    task free

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Nothing to import
+"""
+
+
+class Squarre:
+    """
+
+    """
+    def __init__(self, size=0):
+        """
+        """
+        self.__size = size
+        try:
+            assert type(size) == int
+        except:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
