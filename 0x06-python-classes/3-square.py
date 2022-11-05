06eabbb131cea3dc34fe9d33f9fe8a7914b30608
kevinDlan <kevinkone19@gmail.com>
2022-11-05 11:55:25 +0000

    compute square area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,15 +9,13 @@ class Square:
     Private instance attribute
     public instance method
     """
-    def init(self,size=0):
+    def init(self, size=0):
         try:
             assert type(size) == int
-        except:
+        except TypeError:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
-
     def area(self):
         """
         public instance method
