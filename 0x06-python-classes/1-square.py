5c6ba55d3628ae1ac5ea39f98ce1ecf9d40ea689
kevinDlan <kevinkone19@gmail.com>
2022-11-05 11:15:59 +0000

    create class with constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Empty comment
+"""
+
+class Square:
+    """
+    Define class constructor
+    """
+    def __init__(self, size):
+        """
+        size: param
+        ----------------------
+        Private attribute size
+        """
+        self.__size = size
