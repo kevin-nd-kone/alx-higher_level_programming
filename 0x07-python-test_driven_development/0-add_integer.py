5e41739ad2416508c92452ebd799d4a5e731650a
kevinDlan <kevinkone19@gmail.com>
2022-11-07 22:12:45 +0000

    tes driven tast 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ Nothing to import
 """
 
 
-def add_integer(a, b = 98):
+def add_integer(a, b=98):
     """
         add two integer
     """
@@ -12,14 +12,11 @@ def add_integer(a, b = 98):
         assert type(a) in (int, float)
     except TypeError:
         raise TypeError('a must be an integer')
-    
     try:
         assert type(b) in (int, float)
     except TypeError:
         raise TypeError('b must be an integer')
-    
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
-
     return a + b
